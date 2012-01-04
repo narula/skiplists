e96a70d46161afb868f651051afed9c8cc11d1a0
Neha Narula <narula@gmail.com>
2012-01-04 15:33:22 -0500

    fix

--- skiplist2.cc
+++ skiplist2.cc
@@ -226,4 +226,3 @@ int main(int argc, char** argv) {
   printf("lookup: %ld; itr: %d; size: %d; levels: %d; probability: %d\n", 
 		 (lookup_time-start)/(ITERATIONS), ITERATIONS, LIST_SIZE, maxl, 2);
 }
-xo
